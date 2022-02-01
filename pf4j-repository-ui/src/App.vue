132e7478b181d42448d33e047f7e9d3c68cceaaf
Benjamin Schroth <info@benjamin-schroth.dev>
2022-02-01 02:01:31 +0100

    Remove background-color

--- pf4j-repository-ui/src/App.vue
+++ pf4j-repository-ui/src/App.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import AppLayout from '@/layouts/AppLayout.vue'
+import AppLayout from "@/layouts/AppLayout.vue";
 </script>
 
 <template>
-  <div id="app" style="background-color: cornsilk">
-    <AppLayout/>
+  <div id="app">
+    <AppLayout />
   </div>
 </template>
-
