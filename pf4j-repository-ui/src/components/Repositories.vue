3b3bab147590386fc4c8aa2d78377c667bd358e9
Benjamin Schroth <info@benjamin-schroth.dev>
2022-02-01 01:59:28 +0100

    Use tables for repository-overview

--- pf4j-repository-ui/src/components/Repositories.vue
+++ pf4j-repository-ui/src/components/Repositories.vue
@@ -1,21 +1,40 @@
 <template>
+  <div class="flex flex-col items-center justify-center gap-4">
     <h3 class="text-2xl font-semibold italic">Repositories</h3>
-    <div class="flex gap-10 mb-10">
-        <div v-for="repo in store.repositories" class="bg-green-300 shadow-xl rounded py-2 px-2 hover:bg-green-500 ">
-        <h2 class="text-xl font-bold">{{repo.name}} </h2> 
-        <p>
-            {{repo.pluginCount}} plugins uploaded
-            </p>
-        </div>
-    </div>
-    <button class="bg-green-300 rounded shadow text-black font-semibold py-2 px-2 hover:bg-green-500">Create new repository</button>
+    <table class="table-auto border-collapse border-slate-400 w-full">
+      <thead class="bg-slate-200">
+        <tr>
+          <th class="border-b border-slate-300">Repository Name</th>
+          <th class="border-b border-slate-300">Plugins</th>
+          <th class="border-b border-slate-300">Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="repo in store.repositories" class="border-b-2">
+          <td class="border-b border-slate-300">{{ repo.name }}</td>
+          <td class="border-b border-slate-300">{{ repo.pluginCount }}</td>
+          <td class="border-b border-slate-300">
+            <button
+              class="bg-red-500 text-white rounded-lg shadow-lg p-2 hover:bg-red-800 hover:scale-110 transition ease-in-out"
+            >
+              <i class="fas fa-trash"></i>
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <button
+      class="self-start bg-gray-500 rounded shadow text-white font-semibold py-2 px-2 hover:bg-gray-300 hover:text-black"
+    >
+      New Repository
+    </button>
+  </div>
 </template>
 
 <script setup lang="ts">
-import useRepositoryStore from '@/stores/repository-store'
+import useRepositoryStore from "@/stores/repository-store";
 
-const store = useRepositoryStore()
+const store = useRepositoryStore();
 
-store.fetch()
-
+store.fetch();
 </script>
